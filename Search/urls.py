e8f2c02d241c4938bab95ef05b11672f215f8c98
Imran Akbar <infotainer04@gmail.com>
2022-11-18 10:30:03 +0500

    style: :art: Created Search Page Frontend with styles

--- Search/urls.py
+++ Search/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.Home, name="Home")
+    path("", views.Home, name="Home"),
+    path("search", views.Search, name="Search"),
 ]
--- Search/views.py
+++ Search/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import request
 
 
-
 # Create your views here.
 def Home(request):
     return render(request, 'home/index.html')
+
+
+def Search(request):
+    return render(request, 'page/search.html')
